87c091ba23eee326bab7fcba767ef7c6ba0580f0
Mateo <mateo.nicoloff25@gmail.com>
2024-02-28 21:19:59 -0300

    Musica Contextual

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -4,10 +4,12 @@ const route = express.Router()
 const usuariosRoutes = require("./usuariosRoutes")
 const artistasRoutes = require("./artistasRoutes")
 const playlistRoutes = require("./playlistRoutes")
+const musicaContextualRoutes = require("./musicaContextualRoutes")
 
 route.use(usuariosRoutes)
 route.use(artistasRoutes)
 route.use(playlistRoutes)
+route.use(musicaContextualRoutes)
 
 
 module.exports = route
