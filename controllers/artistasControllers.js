25304fcb603f2deb19d7ca7e2e98273d27229bbc
Mateo <mateo.nicoloff25@gmail.com>
2024-03-02 19:01:38 -0300

    Cupido Musical v1.0

--- controllers/artistasControllers.js
+++ controllers/artistasControllers.js
@@ -10,10 +10,28 @@ const artistasRandom = async (req, res) => {
     res.status(200).json(result)
 }
 
+const verArtistasSeleccionados = async(req, res) => {
+    const listaArtistas = req.body.listaSeleccionados;
+    let fotosArtistas = []
+
+    for (let i=0; i<listaArtistas.length; i++){
+        let numero = parseInt(listaArtistas[i])
+        let foto = await knex("artistas").select("imagen").where("id", "=", numero )
+        
+
+        for (let f=0 ; f<foto.length; f++){
+            fotosArtistas.push(foto[f])   
+        }
+    }
+
+    res.json(fotosArtistas)
+}
+
 /* SELECT * FROM your_table ORDER BY RAND() LIMIT 1; */
 
 module.exports = {
     verArtistas,
-    artistasRandom
+    artistasRandom,
+    verArtistasSeleccionados
 
 }
--- routes/artistasRoutes.js
+++ routes/artistasRoutes.js
@@ -7,5 +7,7 @@ const verificarToken = require("../middlewares/authUser")
 
 route.get("/verArtistas",/* verificarToken.verificarToken  ,*/artistasControllers.verArtistas)
 route.get("/artistasRandom", verificarToken.verificarToken, artistasControllers.artistasRandom)
+route.post("/verArtistasSeleccionados", verificarToken.verificarToken, artistasControllers.verArtistasSeleccionados)
+
 
 module.exports = route;
