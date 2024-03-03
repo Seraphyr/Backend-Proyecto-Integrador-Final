c389a2960e873f593a6818e9032c964112446bb0
Mateo <mateo.nicoloff25@gmail.com>
2024-03-03 20:28:00 -0300

    duracion playlist

--- routes/playlistRoutes.js
+++ routes/playlistRoutes.js
@@ -7,5 +7,6 @@ const verificarToken = require("../middlewares/authUser")
 route.post("/crearCupidoMusical", verificarToken.verificarToken, playlistControllers.crearCupidoMusical)
 route.post("/crearMusicaContextual", verificarToken.verificarToken, playlistControllers.crearMusicaContextual)
 route.get("/verPlaylistUsuario", verificarToken.verificarToken, playlistControllers.verPlaylistUsuario)
+route.post("/verDuracionPlaylist", verificarToken.verificarToken, playlistControllers.verDuracionPlaylist)
 
 module.exports = route; 
